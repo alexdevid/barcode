a3c0ee7066bafdca0cbc3503f91c543fb0711100
Alexander Kovalenko <alexanderk23@gmail.com>
2014-10-21 15:25:20 +0400

    Add cap task to update composer deps; fix migrate

--- protected/config/deploy.rb
+++ protected/config/deploy.rb
@@ -25,11 +25,26 @@ set :copy_exclude, [".git", ".gitignore", "protected/config/db.php"]
 # Set here the files which should stay the same between releases
 set :shared_children, %w(assets protected/runtime vendor userfiles)
 
+set :composer_path, "#{shared_path}"
+set :composer_file, "#{composer_path}/composer.phar"
+
+namespace :composer do
+  task :update do
+    # run "ln -nfs #{shared_path}/web/uploads #{release_path}/web/uploads"
+    run "[ -e #{composer_file} ] || curl -s http://getcomposer.org/installer | php -- --install-dir=#{composer_path}"
+    run "cd #{release_path} && php #{composer_file} install"
+  end
+end
+
 namespace :deploy do
   # Override default rails migrations in case you use them
   #task :migrate do
   #  run "#{latest_release}/yii migrate --interactive=0"
   #end
+
+  task :migrate do
+    run "#{latest_release}/protected/yiic migrate --interactive=0"
+  end
 
   # If you use migrations and don't want to run `cap deploy:migrations` every time
   # uncomment following 3 lines
@@ -38,10 +53,10 @@ namespace :deploy do
   #end
 
   # Install composer dependencies (replace `cd` with something smarter if you wish)
-  task :composer do
-    run "cd #{latest_release} && php composer.phar install"
-    run "protected/yiic migrate"
-  end
+  # task :composer do
+  #  run "cd #{latest_release} && php #{composer_file} install"
+  #  run "protected/yiic migrate"
+  # end
 
   # Make shared_children directories writable by others in case web server isn't in your group
   # By default capistrano sets 0775 mode for those directories
@@ -52,4 +67,4 @@ namespace :deploy do
 end
 
 after "deploy:setup", "deploy:after_setup"
-after "deploy:finalize_update", "deploy:composer"
+after "deploy:finalize_update", "composer:update", "deploy:migrate"
